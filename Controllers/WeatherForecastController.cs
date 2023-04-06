d8164f98937f7c040d99bdba949b649923ac5012
Don Rolling <don.rolling@caliberfs.com>
2023-04-06 13:35:59 -0500

    update settings

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -12,8 +12,8 @@ namespace APIM_Prototype.Controllers
 	{
 		private static readonly string[] Summaries = new[]
 		{
-		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-	};
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+		};
 
 		private readonly ILogger<WeatherForecastController> _logger;
 
